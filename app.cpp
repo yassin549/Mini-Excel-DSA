87b01c8a2187903e8fba9e363fb951f5a7f6f44e
yassin549 <khoualdiyassin26@gmail.com>
2020-03-01 11:30:00 +0500

    Set up linked list data structure for cells

--- app.cpp
+++ app.cpp
@@ -88,3 +88,99 @@ class Excel {
             }
 
             temp = head;
+            
+            // now making the rest of the grid
+            Cell<T> *prevPointer = head;
+            Cell<T> *currPointer = head->right;
+            
+            for (int i = 1; i < cols; i++){
+                Cell<T> *prev = prevPointer->down;
+                Cell<T> *curr = currPointer;
+                for (int j = 1; j < rows; j++){
+                    Cell<T> *newCell = new Cell<T>();
+                    prev->right = newCell;
+                    newCell->left = prev;
+                    prev = prev->down;
+                    curr->down = newCell;
+                    newCell->up = curr;
+                    curr = newCell;
+                }
+                prevPointer = currPointer;
+                currPointer = currPointer->right;
+            }
+        }
+
+        void insertAbove(){
+
+            // head onto the first cell of the selected row
+            Cell<T> *rowHead = selected;
+            while(rowHead->left != nullptr){
+                rowHead = rowHead->left;
+            }
+
+            if (rowHead->up){
+                // if there is a row above the selected row
+
+                Cell<T> *prevRowHead = rowHead->up;
+                Cell<T> *prevPointer = prevRowHead->right;
+                Cell<T> *currPointer = rowHead->right;
+
+                Cell <T> *newRowHead = new Cell<T>();
+                prevRowHead->down = newRowHead;
+                newRowHead->up = prevRowHead;
+                newRowHead->down = rowHead;
+                rowHead->up = newRowHead;
+
+                Cell<T> *newRowPointer = newRowHead;
+
+                for (int i = 1; i < cols; i++){
+
+                    Cell<T> *newCell = new Cell<T>();
+                    newRowPointer->right = newCell;
+                    newCell->left = newRowPointer;
+
+                    prevPointer->down = newCell;
+                    newCell->up = prevPointer;
+
+                    currPointer->up = newCell;
+                    newCell->down = currPointer;
+
+                    newRowPointer = newCell;
+                    prevPointer = prevPointer->right;
+                    currPointer = currPointer->right;
+
+                }
+            }
+            else{
+                // this is the first row
+                makeNewRowAtTop();
+            }
+            rows++;
+        }
+
+        void makeNewRowAtTop(){
+            Cell<T> *newRowHead = new Cell<T>();
+            Cell<T> *temp = head->right;
+            head->up = newRowHead;
+            newRowHead->down = head;
+
+            Cell<T> *curr = newRowHead;
+            for (int i = 1; i < cols; i++){
+                Cell<T> *newCell = new Cell<T>();
+                newCell->left = curr;
+                curr->right = newCell;
+                temp->up = newCell;
+                newCell->down = temp;
+
+                temp = temp->right;
+                curr = newCell;
+            }
+
+            head = newRowHead;
+        }
+
+        void insertBelow(){
+
+            // head onto the first cell of the selected row
+            Cell<T> *rowHead = selected;
+            while(rowHead->left != nullptr){
